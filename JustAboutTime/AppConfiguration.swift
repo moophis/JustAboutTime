690a10f44f4c3cf47255de84e6415ba24dbd85d8
moophis <moophis@gmail.com>
2026-04-26 22:22:53 -0700

    fix(defaults): change default 3rd preset from 50min to 45min

--- JustAboutTime/AppConfiguration.swift
+++ JustAboutTime/AppConfiguration.swift
@@ -4,7 +4,7 @@ import Foundation
 enum AppConfiguration {
     static let appDisplayName = "Just About Time"
     static let menuBarSystemImage = "timer"
-    static let defaultPresetDurations: [TimeInterval] = [5 * 60, 25 * 60, 50 * 60]
+    static let defaultPresetDurations: [TimeInterval] = [5 * 60, 25 * 60, 45 * 60]
     static let minimumPresetDuration: TimeInterval = 1
     static let maximumPresetDuration: TimeInterval = 24 * 60 * 60
     static let startPauseShortcutName = AppShortcuts.startPauseTimer
